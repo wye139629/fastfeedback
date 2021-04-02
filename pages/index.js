802dd6a287576c359627b37bd2e839e2103de728
will <wye139629@gmail.com>
2021-04-02 17:50:58 +0800

    apply ui and set up firestore site data

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import firebase from 'lib/firebase'
 import { auth } from 'firebase'
 import { useAuth } from 'lib/auth'
-import { Button, Heading, Text } from '@chakra-ui/react'
+import { Button, Heading, Text, Flex } from '@chakra-ui/react'
+import Image from 'next/image'
 
 export default function Home() {
   const auth = useAuth()
@@ -13,22 +14,27 @@ export default function Home() {
         <title>Fast feed back</title>
       </Head>
 
-      <Heading fontWeight='400'>
-        Fast feedback
-      </Heading>
-      <Text>
-        current user: {auth.user ? auth.user.email : ''}
-      </Text>
-      {
-        auth.user ?
-        <div>
-          <Button colorScheme="teal" variant="solid" onClick={(e) => auth.signout()}>Signout</Button>
-        </div>
-        :
-        <div>
-          <Button colorScheme="teal" variant="outline" onClick={(e)=> auth.gitHubSignin()}>Signin</Button>
-        </div>
-      }
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        justify="center"
+        h="100vh"
+      >
+        <Image src='/img/logo.png' width='50px' height='50px'/>
+        {
+          auth.user ?
+          <div>
+            <Button mt={4} as="a" href="/dashboard">
+              View Dashboard
+            </Button>
+          </div>
+          :
+          <div>
+            <Button mt={4} colorScheme="teal" variant="outline" onClick={(e)=> auth.gitHubSignin()}>Signin</Button>
+          </div>
+        }
+      </Flex>
     </div>
   )
 }
