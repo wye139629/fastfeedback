fc664870a268275f981763cf1ef9c0ebda94ac70
William Liu <63534298+wye139629@users.noreply.github.com>
2021-04-04 18:18:11 +0800

    fix syntax (#8)

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,10 +1,8 @@
-import { useAuth } from 'lib/auth';
 import fetcher from 'utils/fetcher';
 import useSWR from 'swr';
 import DashboardShell from 'components/DashboardShell';
 import EmptyState from 'components/EmptyState';
 import DataTable from 'components/DataTable';
-import { useState, useEffect } from 'react'
 
 const Dashboard = () => {
   const { data, error } = useSWR('/api/sites', fetcher);
